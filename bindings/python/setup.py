3c00a9be8598a97cc748d89bbcaccce83cb0546c
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-04 12:02:23 -0500

    Updated version number

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -9,7 +9,7 @@ with open('./docs/pypi_docs.md', 'r') as f:
 
 setup(
     name='STRling',
-    version='2.3.1',
+    version='2.4.0',
     packages=find_packages(),
     install_requires=[
         # Add dependencies here.
