76423b431b917ba8cbf7820d6bd32c56f06c7061
Timothy Macfarlane <timdiscovers@gmail.com>
2025-09-06 14:18:45 -0500

    Add Python dependencies and CI configuration for bindings

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,19 +1,27 @@
-# pip install setuptools wheel twine
-# python setup.py sdist bdist_wheel
-# twine upload dist/*
+from pathlib import Path
 from setuptools import setup, find_packages
 
-with open('./docs/pypi_docs.md', 'r') as f:
-    description = f.read()
+here = Path(__file__).parent
+readme = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='STRling',
     version='2.5.4',
     packages=find_packages(),
-    install_requires=[
-        # Add dependencies here.
-        # e.g. 'numpy>=1.11.1'
-    ],
-    long_description=description,
+    install_requires=[],
+    long_description=readme,
     long_description_content_type='text/markdown',
+    url="https://github.com/TheCyberLocal/STRling",
+    project_urls={
+        "Homepage": "https://github.com/TheCyberLocal/STRling",
+        "Documentation": "https://github.com/TheCyberLocal/STRling/tree/main/docs",
+        "Issues": "https://github.com/TheCyberLocal/STRling/issues",
+    },
+    author="TheCyberLocal",
+    license="MIT",  # SPDX identifier; keeps setuptools happy
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
 )
