3b22facfb229075234a4f48d657fe180dc8d3a06
TheCyberLocal <timdiscovers@gmail.com>
2025-09-09 23:08:55 -0500

    chore: add package.json with TypeScript and Jest devDependencies

--- bindings/javascript/jest.config.mjs
+++ bindings/javascript/jest.config.mjs
@@ -1,5 +1,6 @@
 // jest.config.mjs
 export default {
+    preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
     // Only list extensions Jest *can't* infer as ESM from package.json
